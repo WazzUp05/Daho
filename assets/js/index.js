3cf749c690d34690791b4039f1f4f364ea6a47bd
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2023-02-02 16:44:52 +0600

    Добавил страницу cart

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,6 +36,43 @@ $(document).ready(function () {
             // instead of a settings object
         ],
     });
+    $('.cart-product__product-slider').slick({
+        dots: true,
+        autoplay: true,
+        fade: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
+    });
     $('.popular__item-slider').slick({
         dots: true,
         autoplay: false,
@@ -74,4 +111,19 @@ $(document).ready(function () {
             // instead of a settings object
         ],
     });
+    const textElement = document.getElementById('text');
+    const toggleButton = document.getElementById('toggleButton');
+
+    toggleButton.addEventListener('click', function () {
+        if (textElement.style.overflow !== 'visible') {
+            textElement.style.overflow = 'visible';
+            textElement.style.WebkitLineClamp = 'none';
+            toggleButton.innerHTML = 'Show Less';
+        } else {
+            textElement.style.overflow = 'hidden';
+            textElement.style.WebkitLineClamp = '3';
+            toggleButton.innerHTML = 'Show More';
+        }
+    });
 });
+// Напиши скрипт который будет скрывать текст до 3х строк, а при нажатии на кнопку показывать полностью
