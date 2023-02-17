a447f129c7a2ae288c5179d2ae713bf7feb76b39
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2023-02-17 18:40:40 +0600

    обновил дизайн

--- assets/js/index.js
+++ assets/js/index.js
@@ -73,6 +73,79 @@ $(document).ready(function () {
             // instead of a settings object
         ],
     });
+    $('.stock-slider').slick({
+        dots: false,
+        autoplay: true,
+        infinite: true,
+        swipeToSlide: true,
+
+        speed: 300,
+        slidesToShow: 4,
+        centerMode: true,
+        // variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
+    });
+    $('.last-view-slider').slick({
+        dots: false,
+        autoplay: true,
+        infinite: true,
+        speed: 300,
+        swipeToSlide: true,
+        slidesToShow: 6,
+        // centerMode: true,
+        // variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
+    });
     $('.popular__item-slider').slick({
         dots: true,
         autoplay: false,
@@ -221,4 +294,20 @@ $(document).ready(function () {
     $('#card-number').mask('0000 0000 0000 0000');
     $('#valid-until').mask('00/00');
     $('#cvv').mask('000');
+
+    let clearInput = $('.header__search-clear'); // Кнопка очистки
+    let searchInput = $('.search'); // Инпут поиска
+
+    // Показ иконки очистки для поиска
+    searchInput.bind('input', function () {
+        if (searchInput.val().length) {
+            clearInput.show();
+        } else clearInput.hide();
+    });
+
+    // Очистка поиска
+    clearInput.on('click', function () {
+        searchInput.val('');
+        clearInput.hide();
+    });
 });
