a634a509101c6d6c6d4085563b21ffb8131f5d42
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2023-02-27 14:37:34 +0600

    27

--- assets/js/index.js
+++ assets/js/index.js
@@ -92,11 +92,11 @@ $(document).ready(function () {
                 },
             },
             {
-                breakpoint: 600,
+                breakpoint: 767,
                 settings: {
                     arrows: false,
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 },
             },
             {
@@ -119,6 +119,46 @@ $(document).ready(function () {
         speed: 300,
         swipeToSlide: true,
         slidesToShow: 6,
+        // centerMode: true,
+        // variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    centerMode: true,
+                    arrows: true,
+                    slidesToShow: 4,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    centerMode: true,
+                    arrows: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
+    });
+    $('.similar-products-slider').slick({
+        dots: false,
+        autoplay: true,
+        infinite: true,
+        speed: 300,
+        swipeToSlide: true,
+        slidesToShow: 5,
         // centerMode: true,
         // variableWidth: true,
         responsive: [
@@ -199,13 +239,12 @@ $(document).ready(function () {
         });
     }
 
-    $('.cart-product__options-option').click(function () {
-        $('.cart-product__options-option').removeClass('active');
-        $(this).addClass('active');
+    $('.cart-product__options ').on('click', '.cart-product__option', function () {
+        $(this).addClass('active').siblings().removeClass('active');
     });
 
-    $('.cart-product__options-color').click(function () {
-        $('.cart-product__options-color').removeClass('active');
+    $('.cart-product__color').click(function () {
+        $('.cart-product__color').removeClass('active');
         $(this).addClass('active');
     });
 
